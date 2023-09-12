fcaec3dfc894afd1562b47ebd3913ba61ca22be4
Kauã Machado Grathwohl <kaua.machado@emasoftware.com.br>
2023-09-12 11:49:02 -0300

    Adicionando JavaScript ao chat

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-function adicionaLog(){
-  console.log('Teste')
+function adicionarMensagem(nome, texto, classe) {
+
+    let divNome = document.createElement("div");
+    let divTexto = document.createElement("div");
+
+    divNome.className = "msg-" + classe;
+    divTexto.className = "msg mensagem-" + classe;
+
+    divNome.innerHTML = "<strong>" + nome + " diz:</strong>";
+    divTexto.textContent = texto;
+
+    let chatMensagem = document.querySelector(".chat-mensagem");
+
+    chatMensagem.appendChild(divNome);
+    chatMensagem.appendChild(divTexto);
+}
+
+function enviarMensagem(){
+
+    let inputMensagem = document.getElementById("inputMensagem");
+    let mensagem = inputMensagem.value;
+
+    if (mensagem.trim() !== "") {
+
+        adicionarMensagem("Você", mensagem, "aluno");
+        inputMensagem.value = "";
+    }
+    else {
+        alert('Digite uma mensagem!');
+    }
 }
